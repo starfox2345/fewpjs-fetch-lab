47eea9a6feb69cd818615e7326d4b08e15b8cb65
yatsun <yatsunlam25@gmail.com>
2021-08-30 21:52:20 -0400

    commit tree 4f814913b9e07ddd61f37ad9e58692853b87c5b9

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
+  let findBooks = fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
+  return findBooks
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+
+function renderBooks(findBooks) {
+  const main = document.querySelector('main')
+  findBooks.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = book.name
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+})
